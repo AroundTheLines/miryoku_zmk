8cc45e7fa61967f98fc4a41c0021b7e137b8bb08
Qile Wang <AroundTheLines@users.noreply.github.com>
2023-02-06 01:20:22 -0500

    Add swweeep config file

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -4,29 +4,29 @@
 
 #pragma once
 
-#if !defined (MIRYOKU_LAYER_LIST)
+#if !defined(MIRYOKU_LAYER_LIST)
 
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base",   LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(EXTRA,  "Extra",  LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(TAP,    "Tap",    LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(BUTTON, "Button", LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(NAV,    "Nav",    LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(MOUSE,  "Mouse",  LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(MEDIA,  "Media",  LEFT_ARROW, RIGHT_ARROW) \
-MIRYOKU_X(NUM,    "Num",    UP_ARROW,  DOWN_ARROW) \
-MIRYOKU_X(SYM,    "Sym",    UP_ARROW,  DOWN_ARROW) \
-MIRYOKU_X(FUN,    "Fun",    UP_ARROW,  DOWN_ARROW)
+#define MIRYOKU_LAYER_LIST      \
+    MIRYOKU_X(BASE, "Base")     \
+    MIRYOKU_X(EXTRA, "Extra")   \
+    MIRYOKU_X(TAP, "Tap")       \
+    MIRYOKU_X(BUTTON, "Button") \
+    MIRYOKU_X(NAV, "Nav")       \
+    MIRYOKU_X(MOUSE, "Mouse")   \
+    MIRYOKU_X(MEDIA, "Media")   \
+    MIRYOKU_X(NUM, "Num")       \
+    MIRYOKU_X(SYM, "Sym")       \
+    MIRYOKU_X(FUN, "Fun")
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
+#define U_BASE 0
+#define U_EXTRA 1
+#define U_TAP 2
 #define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_NAV 4
+#define U_MOUSE 5
+#define U_MEDIA 6
+#define U_NUM 7
+#define U_SYM 8
+#define U_FUN 9
 
 #endif
